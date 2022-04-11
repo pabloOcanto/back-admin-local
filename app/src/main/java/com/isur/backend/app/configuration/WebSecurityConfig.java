d12f7180cdffc6603f765885a4a09903ddb5f9df
pabloOcanto <pomalianni@gmail.com>
2022-04-10 22:01:55 -0300

    fix cors

--- app/src/main/java/com/isur/backend/app/configuration/WebSecurityConfig.java
+++ app/src/main/java/com/isur/backend/app/configuration/WebSecurityConfig.java
@@ -1,17 +1,12 @@
 package com.isur.backend.app.configuration;
 
 import com.isur.backend.app.filters.JWTAuthorizationFilter;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -28,21 +23,5 @@ public class WebSecurityConfig  extends WebSecurityConfigurerAdapter {
                 .antMatchers("/v1/user/oauth/token").permitAll()
                 .anyRequest().authenticated();
     }
-
-    @Bean
-    CorsConfigurationSource corsConfigurationSource()
-    {
-        CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
-        corsConfiguration.setAllowedOrigins(Arrays.asList("*"));
-        corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PUT","OPTIONS","PATCH", "DELETE"));
-        corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.setExposedHeaders(Arrays.asList("Authorization"));
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", corsConfiguration);
-        return source;
-    }
-
 
 }
--- app/src/main/resources/data.sql
+++ app/src/main/resources/data.sql
@@ -6,9 +6,9 @@ INSERT INTO user (id, dni,email,password,mobile_phone,full_name,status,rol) VALU
 INSERT INTO notification (id,topic,title,area,message,date_created,user_created_id,status) VALUES (null, 'level-1','accidente ruta 3','Rio Negro,Rawson;Neuquen,San Martin de los Andes','ruta cortada por 3 hs','2022-02-19T16:23:00',1,'SENDED');
 
 
-INSERT INTO topic (id, name,status) VALUES (null, 'level-1','ENABLED');
-INSERT INTO topic (id, name,status) VALUES (null, 'level-2','ENABLED');
-INSERT INTO topic (id, name,status) VALUES (null, 'level-3','DISABLED');
+INSERT INTO topic (id, name,status) VALUES (null, 'peligro','ENABLED');
+INSERT INTO topic (id, name,status) VALUES (null, 'advertencia','ENABLED');
+INSERT INTO topic (id, name,status) VALUES (null, 'informacion','ENABLED');
 
 INSERT INTO city (id, lat,lon,city,state,status) VALUES (null, -0.116773, 51.510357,'Rio Negro','Rawson','ENABLED');
 INSERT INTO city (id, lat,lon,city,state,status) VALUES (null, -0.116773, 51.510357,'Chubut','Puerto Madryn','ENABLED');
